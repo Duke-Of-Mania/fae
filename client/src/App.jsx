175ac00a88b928da353354e5ee786aaa6496598a
Eric DeVille <EricJ.DeVille@gmail.com>
2026-08-19 22:11:40 +0000

    front end checkpoint

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+// Import the landing page component we created.
+import LandingPage from "./pages/landingpage";
+
+// This component is the main entry point for our application.
+// It determines what content React should display.
+function App() {
+  // Display the landing page.
+  return <LandingPage />;
+}
+
+// Export App so the React application can use it.
+export default App;
